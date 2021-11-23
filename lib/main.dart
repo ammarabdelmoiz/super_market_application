50871151259d9b4de79963186784d7c83a2fc30e
Ammar Abdelmo3z <ammar.abdelmoiz44@gmail.com>
2021-11-23 23:11:56 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Super Market',
+      title: 'Super Market Application',
       initialRoute: '/',
       routes: {
         '/': (context) => Signin(),
